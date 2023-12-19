9fdd2a24d002031ccc40eeb7c546b4f9a1f33646
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-19 14:25:57 +0530

    Add project files.

---
+++ Getri_PartialViews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Getri_PartialViews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Getri_PartialViews/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Getri_PartialViews.Models
+{
+    public class Products
+    {
+        public int ProductId { get; set; }
+                
+        public string ProductName { get; set; }
+
+        public string ProductCategory { get; set; }
+
+        public string Description { get; set; }
+                
+        public decimal ProductPrice { get; set; }
+    }
+}
